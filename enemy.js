9c5aa51fcb9bca6c50006ebaa0036055b3962b26
KUBAT <45283140+kubat02@users.noreply.github.com>
2024-12-06 09:59:15 +0900

    Update enemy.js

--- enemy.js
+++ enemy.js
@@ -1,10 +1,8 @@
-function drawEnemies(ctx) {
-  currentMap.enemies.forEach((enemy) => {
-    ctx.fillStyle = enemy.color;
-    ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
-  });
-}
-
 function updateEnemies() {
-  // Düşman davranışları eklenecek
+  currentMap.enemies.forEach((enemy) => {
+    enemy.x += enemy.dx; // Yatay hareket
+    if (enemy.x < 0 || enemy.x + enemy.width > 800) {
+      enemy.dx *= -1; // Ekran sınırlarında yön değiştir
+    }
+  });
 }
